5992d669c9e52804498c2548d1d13c9216f1fc6e
mihotaka9116 <mihotaka9116@gmail.com>
2026-02-13 18:41:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,19 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       // 画面内に入ったら .is-show クラスを付与
       entry.target.classList.add('is-show');
+      
+      // 一度表示されたら監視をやめる
+      observer.unobserve(entry.target);
     }
   });
 }, {
   root: null,
-  rootMargin: '0px',
-  threshold: 0.2 // 20%見えたら実行
+  rootMargin: '0px -50px', // 左右少し内側で反応するように調整
+  threshold: 0.1 // 10%見えたら実行
 });
 
 // .js-fadein がついた要素をすべて監視する
 const fadeElements = document.querySelectorAll('.js-fadein');
 fadeElements.forEach((el) => {
   observer.observe(el);
-});
-
-window.addEventListener('scroll', function() {
-  const fadeins = document.querySelectorAll('.js-fadein');
-  for (let i = 0; i < fadeins.length; i++) {
-    const rect = fadeins[i].getBoundingClientRect().top;
-    const scroll = window.pageYOffset || document.documentElement.scrollTop;
-    const offset = rect + scroll;
-    const windowHeight = window.innerHeight;
-    if (scroll > offset - windowHeight + 150) {
-      fadeins[i].classList.add('is-show');
-    }
-  }
 });
